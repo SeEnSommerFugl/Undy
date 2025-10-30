843d28f65d34888c9e0bfce929618c0fa1b25b6d
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-10-30 14:20:34 +0100

    Updated Model class

--- Undy/Models/PurchaseOrder.cs
+++ Undy/Models/PurchaseOrder.cs
@@ -5,7 +5,9 @@
         public Guid PurchaseOrderID { get; set; }
         public int ProductNumber { get; set; }
         public int Quantity { get; set; }
-        public DateTime DeliveryDate { get; set; }
+        public DateOnly ExpectedDeliveryDate { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public DateOnly DeliveryDate { get; set; }
         public string OrderStatus { get; set; }
         public Guid ProductID { get; set; }
     }
