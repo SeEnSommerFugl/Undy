a17a95ea82bac6729c49e21a2630bdef950d1878
Patrick Kokholm <patrickkokholm@gmail.com>
2025-11-27 10:11:12 +0100

    Started to implement - TempSalesOrderView - TempSalesOrderViewModel - SalesOrderService

---
+++ Undy/ViewModels/Helpers/SalesOrderService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Undy.Data.Repository;
+using Undy.Models;
+
+namespace Undy.ViewModels.Helpers {
+    internal class SalesOrderService {
+        private readonly SalesOrderDBRepository _salesOrderRepo;
+        //private readonly ProductSalesOrderDBRepository _productSalesOrderRepo;
+        //private SqlConnection con = Db.OpenConnection();
+
+        public SalesOrderService() {
+            _salesOrderRepo = new SalesOrderDBRepository(con);
+        }
+
+        public void CreateSalesOrderWithProducts(SalesOrder salesOrder, List<ProductSalesOrder> productSalesOrder) {
+            using(var transaction = con.BeginTransaction()) {
+                try {
+                    _salesOrderRepo.AddAsync(salesOrder);
+
+                    foreach (var product in productSalesOrder) {
+                        product.SalesOrderID = salesOrder.SalesOrderID;
+                        _ProductSalesOrderDBRepository.Add(product);
+                    }
+                    transaction.Commit();
+                } catch (Exception) {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
