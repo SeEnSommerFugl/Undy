bd26841d3f2b24d2f51602499322b8a503ec99f5
David Armstrong <proxie89@gmail.com>
2025-12-01 11:00:58 +0100

    fixed-naming-inconsistency-VM

--- Undy/ViewModels/TestWholesaleOrderViewModel.cs
+++ Undy/ViewModels/TestWholesaleOrderViewModel.cs
@@ -9,9 +9,9 @@ namespace Undy.ViewModels
 {
     public class TestWholesaleOrderViewModel : BaseViewModel
     {
-        private readonly IBaseRepository<TestPurchaseOrder, Guid> _testPurchaseOrderRepo;
+        private readonly IBaseRepository<TestWholesaleOrder, Guid> _testPurchaseOrderRepo;
         private readonly IBaseRepository<Product, Guid> _productRepo;
-        public TestWholesaleOrderViewModel(IBaseRepository<TestPurchaseOrder, Guid> testPurchaseOrderRepo, IBaseRepository<Product, Guid> productRepo)
+        public TestWholesaleOrderViewModel(IBaseRepository<TestWholesaleOrder, Guid> testPurchaseOrderRepo, IBaseRepository<Product, Guid> productRepo)
         {
             _testPurchaseOrderRepo = testPurchaseOrderRepo;
             _productRepo = productRepo;
