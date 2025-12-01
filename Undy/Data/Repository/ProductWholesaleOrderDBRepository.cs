10f81474b9e6058e8f17926978f2e39c99015c90
Patrick Kokholm <patrickkokholm@gmail.com>
2025-12-01 11:26:54 +0100

    trying to fix weird namespace error

--- Undy/Data/Repository/ProductWholesaleOrderDBRepository.cs
+++ Undy/Data/Repository/ProductWholesaleOrderDBRepository.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Undy.Models;
 
 namespace Undy.Data.Repository {
-    internal class ProductWholesaleOrderDBRepository : BaseDBRepository<ProductWholsesaleOrder, Guid> {
+    internal class ProductWholesaleOrderDBRepository : BaseDBRepository<ProductWholesaleOrder, Guid> {
+        protected override Guid GetKey(ProductWholesaleOrder entity) {
+            throw new NotImplementedException();
+        }
+
+        protected override ProductWholesaleOrder Map(IDataRecord r) {
+            throw new NotImplementedException();
+        }
     }
 }
