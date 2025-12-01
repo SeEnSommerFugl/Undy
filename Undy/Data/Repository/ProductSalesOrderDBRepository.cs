0961f99536a17a67b1cb6378c4ded053983f076e
Patrick Kokholm <patrickkokholm@gmail.com>
2025-12-01 11:05:05 +0100

    implemented repositories for productsale and productwholesale

--- Undy/Data/Repository/ProductSalesOrderDBRepository.cs
+++ Undy/Data/Repository/ProductSalesOrderDBRepository.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Undy.Models;
 
 namespace Undy.Data.Repository {
-    internal class ProductSalesOrderDBRepository {
+    internal class ProductSalesOrderDBRepository : BaseDBRepository<ProductSalesOrder, Guid> {
     }
 }
