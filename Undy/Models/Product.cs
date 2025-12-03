3ce2aa97ad28112acbd998c8a7b9f50c85568d90
Patrick Kokholm <patrickkokholm@gmail.com>
2025-12-03 11:48:47 +0100

    Changed ProductNumber datatype from int to string

--- Undy/Models/Product.cs
+++ Undy/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public Guid ProductID { get; set; }
-        public int ProductNumber { get; set; }
+        public string ProductNumber { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public string Size { get; set; }
