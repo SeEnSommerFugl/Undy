5d00d3c6661beabffde8b04f1a21c4bebd49c75a
David Armstrong <proxie89@gmail.com>
2025-12-03 13:34:34 +0100

    added-1.0-paymentview started-VM

--- Undy/ViewModels/PaymentViewModel.cs
+++ Undy/ViewModels/PaymentViewModel.cs
@@ -1,15 +1,67 @@
-﻿using Undy.Data.Repository;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Undy.Data.Repository;
 using Undy.Models;
+
 
 namespace Undy.ViewModels
 {
     public class PaymentViewModel : BaseViewModel
     {
-        private IBaseRepository<SalesOrder, Guid> _salesOrderRepo;
+        private readonly IBaseRepository<SalesOrder, Guid> _salesOrderRepo;
+
+        // ----- backing fields -----
+
+        private string _orderNumber;
+        private string _customerName;
+        private decimal _totalAmount;
+        private decimal _paymentAmount;
+
+        private ObservableCollection<SalesOrderLineViewModel> _orderLines;
+
+        private ObservableCollection<string> _paymentMethods;
+        private string _selectedPaymentMethod;
+
+        private ObservableCollection<string> _statusOptions;
+        private string _selectedStatus;
+
+        private string _statusMessage;
+
+        private Guid _currentSalesOrderId;
+
+        // ----- Constructor -----
 
         public PaymentViewModel(IBaseRepository<SalesOrder, Guid> salesOrderRepo)
         {
             _salesOrderRepo = salesOrderRepo;
+
+            OrderLines = new ObservableCollection<SalesOrderLineViewModel>();
+
+            // 
+            PaymentMethods = new ObservableCollection<string>
+            {
+                "Kort",
+                "MobilePay",
+                "Kontant",
+                "Bankoverførsel"
+            };
+
+            StatusOptions = new ObservableCollection<string>
+            {
+                "Ny",
+                "Under behandling",
+                "Afventer betaling",
+                "Betalt",
+                "Annulleret",
+                "Returneret"
+            };
+
+            ConfirmPaymentCommand = new RelayCommand(
+                execute: _ => ConfirmPayment(),
+                canExecute: _ => CanConfirmPayment()
+            );
         }
-    }
+
+       
 }
