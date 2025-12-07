c6cb5566b613d73a68738612bab1dcdaaa795a40
David Armstrong <proxie89@gmail.com>
2025-12-07 15:13:33 +0100

    Added-WholesaleOrderLine-Model Added-to-IncomingWholeSaleOrderView

---
+++ Undy/Models/WholeSaleOrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undy.Models
+{
+    public class WholeSaleOrderLine
+    {
+        public Guid WholesaleOrderLineID { get; set; }
+
+        public Guid PurchaseOrderID { get; set; }   
+        public Guid ProductID { get; set; }        
+        public int OrderedQuantity { get; set; }    
+        public int ReceivedQuantity { get; set; }
+    }
+}
