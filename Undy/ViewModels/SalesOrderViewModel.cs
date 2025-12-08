d1c0bb42516eb0eb8d22b2203a6d1fa2115cff98
Patrick Kokholm <patrickkokholm@gmail.com>
2025-12-08 13:39:24 +0100

    Implemented SalesOrderView and SalesOrderViewModel

--- Undy/ViewModels/SalesOrderViewModel.cs
+++ Undy/ViewModels/SalesOrderViewModel.cs
@@ -9,58 +9,16 @@ using Undy.ViewModels.Helpers;
 
 namespace Undy.ViewModels
 {
-    public class SalesOrderViewModel : BaseViewModel
-    {
+    public class SalesOrderViewModel : BaseViewModel {
         private readonly IBaseRepository<SalesOrderDisplay, Guid> _salesDisplayRepo;
-
-        private SalesOrderDisplay _selectedSalesOrder;
 
         public ObservableCollection<SalesOrderDisplay> SalesDisplay => _salesDisplayRepo.Items;
         public ICollectionView SaleView { get; }
 
-        
-        public Action<SalesOrderDisplay> PaymentRequested { get; set; }
-
-        public SalesOrderViewModel(IBaseRepository<SalesOrderDisplay, Guid> salesDisplayRepo)
-        {
+        public SalesOrderViewModel(IBaseRepository<SalesOrderDisplay, Guid> salesDisplayRepo) {
             _salesDisplayRepo = salesDisplayRepo;
-
-            SaleView = CollectionViewSource.GetDefaultView(SalesDisplay);
-
-            OpenPaymentCommand = new RelayCommand(
-                _ => OnOpenPayment(),
-                _ => CanOpenPayment()
-            );
-        }
-
-        // ---- Selected item ----
-        public SalesOrderDisplay SelectedSalesOrder
-        {
-            get => _selectedSalesOrder;
-            set
-            {
-                _selectedSalesOrder = value;
-                OnPropertyChanged();
-
-                (OpenPaymentCommand as RelayCommand)?.RaiseCanExecuteChanged();
-            }
-        }
-
-        // ---- Command ----
-        public ICommand OpenPaymentCommand { get; }
-
-        private bool CanOpenPayment()
-        {
-            return SelectedSalesOrder != null;
-        }
-
-        private void OnOpenPayment()
-        {
-            if (SelectedSalesOrder == null)
-                return;
-
-            PaymentRequested?.Invoke(SelectedSalesOrder);
+            SaleView = CollectionViewSource.GetDefaultView(salesDisplayRepo);
+            SaleView.SortDescriptions.Add(new SortDescription("SalesDate", ListSortDirection.Descending));
         }
     }
-
 }
