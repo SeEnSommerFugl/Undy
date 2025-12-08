070f89f769441218491b8ac0fe61912ce5ce9486
Patrick Kokholm <patrickkokholm@gmail.com>
2025-12-08 12:29:43 +0100

    small bug fix

--- Undy/Models/WholesaleOrder.cs
+++ Undy/Models/WholesaleOrder.cs
@@ -1,10 +1,10 @@
 ﻿namespace Undy.Models
 {
-    public class WholesaleOrder
-    {
+    public class WholesaleOrder {
         public Guid PurchaseOrderID { get; set; }
         public DateOnly ExpectedDeliveryDate { get; set; }
         public DateOnly OrderDate { get; set; }
         public DateOnly DeliveryDate { get; set; }
         public string OrderStatus { get; set; }
+    }
 }
