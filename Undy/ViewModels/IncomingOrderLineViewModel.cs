f51d818195af307aaf33b9819769862b4fe30d83
David Armstrong <proxie89@gmail.com>
2025-12-08 17:15:06 +0100

    added-IncomingOrderLineViewModel:

--- Undy/ViewModels/IncomingOrderLineViewModel.cs
+++ Undy/ViewModels/IncomingOrderLineViewModel.cs
@@ -8,6 +8,26 @@ namespace Undy.ViewModels
 {
     public class IncomingOrderLineViewModel : BaseViewModel
     {
-        
+        public Guid PurchaseOrderID { get; set; }
+        public Guid ProductID { get; set; }
+
+        public string ProductNumber { get; set; }   
+        public string ProductName { get; set; }     
+
+        public int OrderedQuantity { get; set; }    
+        public int AlreadyReceived { get; set; }    
+
+        private int _receivedQuantity;
+        public int ReceivedQuantity                 
+        {
+            get => _receivedQuantity;
+            set
+            {
+                if (SetProperty(ref _receivedQuantity, value))
+                    OnPropertyChanged(nameof(Difference));
+            }
+        }
+
+        public int Difference => OrderedQuantity - (AlreadyReceived + ReceivedQuantity);
     }
 }
